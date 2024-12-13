6cee76eba60fa8ad826cdadd4ab04c3ab18436f7
Will Anthony Barillo <143173033+will0704@users.noreply.github.com>
2024-12-14 00:14:06 +0800

    Update 5.0
    Major Update:
    
    - Fixed Splash Screen

--- app/src/main/java/com/example/activity2/SplashActivity.kt
+++ app/src/main/java/com/example/activity2/SplashActivity.kt
@@ -3,6 +3,10 @@ package com.example.activity2
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -15,5 +19,27 @@ class SplashActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        val planeImage: ImageView = findViewById(R.id.planeImage)
+        val splashText: TextView = findViewById(R.id.splashText)
+        val planeAnimation = AnimationUtils.loadAnimation(this, R.anim.plane_move)
+        val textAnimation = AnimationUtils.loadAnimation(this, R.anim.text_slide)
+        planeImage.startAnimation(planeAnimation)
+        splashText.startAnimation(textAnimation) // Set a listener to launch MainActivity after the animation ends
+        planeAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(
+                    Intent(
+                        this@SplashActivity,
+                        MainActivity::class.java
+                    )
+                )
+                finish()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+
     }
 }
